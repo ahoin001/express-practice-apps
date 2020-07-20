3f65d592c852c03f7fd9294a3cb5ba77a65c3d17
Alexander Hoinville <ahoin001@gmail.com>
2020-07-19 20:16:58 -0400

    Set up node to send static assets and files

--- src/app.js
+++ src/app.js
@@ -1,36 +1,44 @@
+// Core modules
+const path = require('path')
+
+// npm modules
 const express = require('express')
 
-// returns application object with several methods used for framework
+
+// returns express application object with several methods used for framework
 app = express();
 
+// Node provided variables for getting absolute path of directory and filename
+console.log(__dirname)
+console.log(__filename)
+            
+// node module: path.join used to start from an absolute path, and move relatively to it to my static assets
+console.log(path.join(__dirname, '../public/'))
+
+const publicDirectory = path.join(__dirname, '../public/')
+
+app.use(express.static(publicDirectory));
+
+
+// ? Express.static provides static assets, html files when typed in browser Ex: localhost:3000/about.html will provide about page
+
+// ? Using send file, html can be provided at specific routes
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname,'../public/about.html'));
+});
 
 // arg1 = route,
 // arg2 = Callback that is provided with req and res object (Object with info of http we recieve, object we use to send something http back)
 
-app.get('/', (req, res) => {
-    res.send('<h1>Weather App!</h1>');
-});
-
-app.get('/help', (req, res) => {
+app.get('/weather', (req, res) => {
 
     // ? To send back json that can be consumed by client
     // ?Automatically stringified by express
     res.send({
-        name: 'Alex',
-        age: 25
-    });
-
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>About Page</h1>');
-});
-
-app.get('/weather', (req, res) => {
-    res.send({
         forecast: 'Sunny',
         location: 'Miami'
     });
+
 });
 
 app.listen(3000, () => {
